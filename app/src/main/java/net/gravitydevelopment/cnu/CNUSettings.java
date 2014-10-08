306e0983a3ac175ca331a0d62f035d1e746a6d35
gravitylow <ad@mfendley.com>
2014-10-08 00:05:48 -0400

    Developer options

--- app/src/main/java/net/gravitydevelopment/cnu/CNUSettings.java
+++ app/src/main/java/net/gravitydevelopment/cnu/CNUSettings.java
@@ -1,16 +1,29 @@
 package net.gravitydevelopment.cnu;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.media.audiofx.BassBoost;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.cengalabs.flatui.FlatUI;
 
 import net.gravitydevelopment.cnu.service.BackendService;
+import net.gravitydevelopment.cnu.service.LocationService;
+import net.gravitydevelopment.cnu.service.SettingsService;
 
 public class CNUSettings extends PreferenceActivity {
+
+    private PrefsFragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +33,40 @@ public class CNUSettings extends PreferenceActivity {
         FlatUI.setDefaultTheme(FlatUI.GRASS);
         getActionBar().setBackgroundDrawable(FlatUI.getActionBarDrawable(this, FlatUI.GRASS, false));
 
+        fragment = new PrefsFragment();
+
         getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new PrefsFragment())
+                .replace(android.R.id.content, fragment)
                 .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.cnusettings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_developer) {
+            PrefsFragment.addDeveloperPrefs();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public static class PrefsFragment extends PreferenceFragment {
 
+        private static Context context;
+        private static PreferenceScreen screen;
+        private static ClipboardManager clipboard;
+
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
+            context = getActivity();
+            clipboard = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
             addPreferencesFromResource(R.xml.preferences);
 
             final CheckBoxPreference wifiOnlyPreference = (CheckBoxPreference) findPreference("preference_wifi_only");
@@ -39,6 +76,41 @@ public class CNUSettings extends PreferenceActivity {
                     boolean value = (Boolean) newValue;
                     BackendService.getSettingsService().setWifiOnly(value);
                     return true;
+                }
+            });
+
+            screen = getPreferenceScreen();
+        }
+
+        public static void addDeveloperPrefs() {
+            Preference id = new Preference(context);
+            id.setTitle("Unique ID");
+            String uuid = SettingsService.getUUID().toString();
+            id.setSummary(uuid);
+            screen.addPreference(id);
+            final ClipData clip = ClipData.newPlainText("CNU Unique ID", uuid);
+            id.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    clipboard.setPrimaryClip(clip);
+                    Toast.makeText(context, "Copied to the clipboard", Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            });
+
+            final Preference loc = new Preference(context);
+            loc.setTitle("Location Data");
+            String name = LocationService.getLastLocation() == null ? "Unknown" : LocationService.getLastLocation().getName();
+            String data = LocationService.getLastLatitude() + "," + LocationService.getLastLongitude() + ": " + name;
+            loc.setSummary(data);
+            screen.addPreference(loc);
+            loc.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    String name = LocationService.getLastLocation() == null ? "Unknown" : LocationService.getLastLocation().getName();
+                    String data = LocationService.getLastLatitude() + "," + LocationService.getLastLongitude() + ": " + name;
+                    loc.setSummary(data);
+                    return false;
                 }
             });
         }
